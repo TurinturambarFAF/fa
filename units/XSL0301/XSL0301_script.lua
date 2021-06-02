019e504018db0d202a50cfab74e48cdeeec4325e
TurinturambarFAF <konrad.schulz@posteo.net>
2021-06-02 20:59:59 +0200

    removing upgrades
    removes all upgrades from Seraphim SACUs before adding multiple different SCUs.

--- units/XSL0301/XSL0301_script.lua
+++ units/XSL0301/XSL0301_script.lua
@@ -45,118 +45,118 @@ XSL0301 = Class(CommandUnit) {
         EffectUtil.CreateSeraphimUnitEngineerBuildingEffects(self, unitBeingBuilt, self.BuildEffectBones, self.BuildEffectsBag)
     end,
 
-    CreateEnhancement = function(self, enh)
-        CommandUnit.CreateEnhancement(self, enh)
-        local bp = self:GetBlueprint().Enhancements[enh]
-        if not bp then return end
-        -- Teleporter
-        if enh == 'Teleporter' then
-            self:AddCommandCap('RULEUCC_Teleport')
-        elseif enh == 'TeleporterRemove' then
-            self:RemoveCommandCap('RULEUCC_Teleport')
-        -- Missile
-        elseif enh == 'Missile' then
-            self:AddCommandCap('RULEUCC_Tactical')
-            self:AddCommandCap('RULEUCC_SiloBuildTactical')
-            self:SetWeaponEnabledByLabel('Missile', true)
-        elseif enh == 'MissileRemove' then
-            self:RemoveCommandCap('RULEUCC_Tactical')
-            self:RemoveCommandCap('RULEUCC_SiloBuildTactical')
-            self:SetWeaponEnabledByLabel('Missile', false)
-        -- Shields
-        elseif enh == 'Shield' then
-            self:AddToggleCap('RULEUTC_ShieldToggle')
-            self:SetEnergyMaintenanceConsumptionOverride(bp.MaintenanceConsumptionPerSecondEnergy or 0)
-            self:SetMaintenanceConsumptionActive()
-            self:CreateShield(bp)
-        elseif enh == 'ShieldRemove' then
-            self:DestroyShield()
-            self:SetMaintenanceConsumptionInactive()
-            self:RemoveToggleCap('RULEUTC_ShieldToggle')
-        -- Overcharge
-        elseif enh == 'Overcharge' then
-            self:AddCommandCap('RULEUCC_Overcharge')
-            self:GetWeaponByLabel('OverCharge').NeedsUpgrade = false
-            self:GetWeaponByLabel('AutoOverCharge').NeedsUpgrade = false
-        elseif enh == 'OverchargeRemove' then
-            self:RemoveCommandCap('RULEUCC_Overcharge')
-            self:SetWeaponEnabledByLabel('OverCharge', false)
-            self:SetWeaponEnabledByLabel('AutoOverCharge', false)
-            self:GetWeaponByLabel('OverCharge').NeedsUpgrade = true
-            self:GetWeaponByLabel('AutoOverCharge').NeedsUpgrade = true
-        -- Engineering Throughput Upgrade
-        elseif enh =='EngineeringThroughput' then
-            if not Buffs['SeraphimSCUBuildRate'] then
-                BuffBlueprint {
-                    Name = 'SeraphimSCUBuildRate',
-                    DisplayName = 'SeraphimSCUBuildRate',
-                    BuffType = 'SCUBUILDRATE',
-                    Stacks = 'REPLACE',
-                    Duration = -1,
-                    Affects = {
-                        BuildRate = {
-                            Add =  bp.NewBuildRate - self:GetBlueprint().Economy.BuildRate,
-                            Mult = 1,
-                        },
-                    },
-                }
-            end
-            Buff.ApplyBuff(self, 'SeraphimSCUBuildRate')
-        elseif enh == 'EngineeringThroughputRemove' then
-            if Buff.HasBuff(self, 'SeraphimSCUBuildRate') then
-                Buff.RemoveBuff(self, 'SeraphimSCUBuildRate')
-            end
-        -- Damage Stabilization
-        elseif enh == 'DamageStabilization' then
-            if not Buffs['SeraphimSCUDamageStabilization'] then
-               BuffBlueprint {
-                    Name = 'SeraphimSCUDamageStabilization',
-                    DisplayName = 'SeraphimSCUDamageStabilization',
-                    BuffType = 'SCUUPGRADEDMG',
-                    Stacks = 'ALWAYS',
-                    Duration = -1,
-                    Affects = {
-                        MaxHealth = {
-                            Add = bp.NewHealth,
-                            Mult = 1.0,
-                        },
-                        Regen = {
-                            Add = bp.NewRegenRate,
-                            Mult = 1.0,
-                        },
-                    },
-                }
-            end
-            if Buff.HasBuff(self, 'SeraphimSCUDamageStabilization') then
-                Buff.RemoveBuff(self, 'SeraphimSCUDamageStabilization')
-            end
-            Buff.ApplyBuff(self, 'SeraphimSCUDamageStabilization')
-          elseif enh == 'DamageStabilizationRemove' then
-            if Buff.HasBuff(self, 'SeraphimSCUDamageStabilization') then
-                Buff.RemoveBuff(self, 'SeraphimSCUDamageStabilization')
-            end
-        -- Enhanced Sensor Systems
-        elseif enh == 'EnhancedSensors' then
-            self:SetIntelRadius('Vision', bp.NewVisionRadius or 104)
-            self:SetIntelRadius('Omni', bp.NewOmniRadius or 104)
-            local wep = self:GetWeaponByLabel('LightChronatronCannon')
-            wep:ChangeMaxRadius(bp.NewMaxRadius or 35)
-            local wep = self:GetWeaponByLabel('OverCharge')
-            wep:ChangeMaxRadius(35)
-            local aoc = self:GetWeaponByLabel('AutoOverCharge')
-            aoc:ChangeMaxRadius(35)
-        elseif enh == 'EnhancedSensorsRemove' then
-            local bpIntel = self:GetBlueprint().Intel
-            self:SetIntelRadius('Vision', bpIntel.VisionRadius or 26)
-            self:SetIntelRadius('Omni', bpIntel.OmniRadius or 16)
-            local wep = self:GetWeaponByLabel('LightChronatronCannon')
-            wep:ChangeMaxRadius(bp.NewMaxRadius or 25)
-            local wep = self:GetWeaponByLabel('OverCharge')
-            wep:ChangeMaxRadius(bp.NewMaxRadius or 25)
-            local aoc = self:GetWeaponByLabel('AutoOverCharge')
-            aoc:ChangeMaxRadius(bp.NewMaxRadius or 25)
-        end
-    end,
+    -- CreateEnhancement = function(self, enh)
+        -- CommandUnit.CreateEnhancement(self, enh)
+        -- local bp = self:GetBlueprint().Enhancements[enh]
+        -- if not bp then return end
+        -- -- Teleporter
+        -- if enh == 'Teleporter' then
+            -- self:AddCommandCap('RULEUCC_Teleport')
+        -- elseif enh == 'TeleporterRemove' then
+            -- self:RemoveCommandCap('RULEUCC_Teleport')
+        -- -- Missile
+        -- elseif enh == 'Missile' then
+            -- self:AddCommandCap('RULEUCC_Tactical')
+            -- self:AddCommandCap('RULEUCC_SiloBuildTactical')
+            -- self:SetWeaponEnabledByLabel('Missile', true)
+        -- elseif enh == 'MissileRemove' then
+            -- self:RemoveCommandCap('RULEUCC_Tactical')
+            -- self:RemoveCommandCap('RULEUCC_SiloBuildTactical')
+            -- self:SetWeaponEnabledByLabel('Missile', false)
+        -- -- Shields
+        -- elseif enh == 'Shield' then
+            -- self:AddToggleCap('RULEUTC_ShieldToggle')
+            -- self:SetEnergyMaintenanceConsumptionOverride(bp.MaintenanceConsumptionPerSecondEnergy or 0)
+            -- self:SetMaintenanceConsumptionActive()
+            -- self:CreateShield(bp)
+        -- elseif enh == 'ShieldRemove' then
+            -- self:DestroyShield()
+            -- self:SetMaintenanceConsumptionInactive()
+            -- self:RemoveToggleCap('RULEUTC_ShieldToggle')
+        -- -- Overcharge
+        -- elseif enh == 'Overcharge' then
+            -- self:AddCommandCap('RULEUCC_Overcharge')
+            -- self:GetWeaponByLabel('OverCharge').NeedsUpgrade = false
+            -- self:GetWeaponByLabel('AutoOverCharge').NeedsUpgrade = false
+        -- elseif enh == 'OverchargeRemove' then
+            -- self:RemoveCommandCap('RULEUCC_Overcharge')
+            -- self:SetWeaponEnabledByLabel('OverCharge', false)
+            -- self:SetWeaponEnabledByLabel('AutoOverCharge', false)
+            -- self:GetWeaponByLabel('OverCharge').NeedsUpgrade = true
+            -- self:GetWeaponByLabel('AutoOverCharge').NeedsUpgrade = true
+        -- -- Engineering Throughput Upgrade
+        -- elseif enh =='EngineeringThroughput' then
+            -- if not Buffs['SeraphimSCUBuildRate'] then
+                -- BuffBlueprint {
+                    -- Name = 'SeraphimSCUBuildRate',
+                    -- DisplayName = 'SeraphimSCUBuildRate',
+                    -- BuffType = 'SCUBUILDRATE',
+                    -- Stacks = 'REPLACE',
+                    -- Duration = -1,
+                    -- Affects = {
+                        -- BuildRate = {
+                            -- Add =  bp.NewBuildRate - self:GetBlueprint().Economy.BuildRate,
+                            -- Mult = 1,
+                        -- },
+                    -- },
+                -- }
+            -- end
+            -- Buff.ApplyBuff(self, 'SeraphimSCUBuildRate')
+        -- elseif enh == 'EngineeringThroughputRemove' then
+            -- if Buff.HasBuff(self, 'SeraphimSCUBuildRate') then
+                -- Buff.RemoveBuff(self, 'SeraphimSCUBuildRate')
+            -- end
+        -- -- Damage Stabilization
+        -- elseif enh == 'DamageStabilization' then
+            -- if not Buffs['SeraphimSCUDamageStabilization'] then
+               -- BuffBlueprint {
+                    -- Name = 'SeraphimSCUDamageStabilization',
+                    -- DisplayName = 'SeraphimSCUDamageStabilization',
+                    -- BuffType = 'SCUUPGRADEDMG',
+                    -- Stacks = 'ALWAYS',
+                    -- Duration = -1,
+                    -- Affects = {
+                        -- MaxHealth = {
+                            -- Add = bp.NewHealth,
+                            -- Mult = 1.0,
+                        -- },
+                        -- Regen = {
+                            -- Add = bp.NewRegenRate,
+                            -- Mult = 1.0,
+                        -- },
+                    -- },
+                -- }
+            -- end
+            -- if Buff.HasBuff(self, 'SeraphimSCUDamageStabilization') then
+                -- Buff.RemoveBuff(self, 'SeraphimSCUDamageStabilization')
+            -- end
+            -- Buff.ApplyBuff(self, 'SeraphimSCUDamageStabilization')
+          -- elseif enh == 'DamageStabilizationRemove' then
+            -- if Buff.HasBuff(self, 'SeraphimSCUDamageStabilization') then
+                -- Buff.RemoveBuff(self, 'SeraphimSCUDamageStabilization')
+            -- end
+        -- -- Enhanced Sensor Systems
+        -- elseif enh == 'EnhancedSensors' then
+            -- self:SetIntelRadius('Vision', bp.NewVisionRadius or 104)
+            -- self:SetIntelRadius('Omni', bp.NewOmniRadius or 104)
+            -- local wep = self:GetWeaponByLabel('LightChronatronCannon')
+            -- wep:ChangeMaxRadius(bp.NewMaxRadius or 35)
+            -- local wep = self:GetWeaponByLabel('OverCharge')
+            -- wep:ChangeMaxRadius(35)
+            -- local aoc = self:GetWeaponByLabel('AutoOverCharge')
+            -- aoc:ChangeMaxRadius(35)
+        -- elseif enh == 'EnhancedSensorsRemove' then
+            -- local bpIntel = self:GetBlueprint().Intel
+            -- self:SetIntelRadius('Vision', bpIntel.VisionRadius or 26)
+            -- self:SetIntelRadius('Omni', bpIntel.OmniRadius or 16)
+            -- local wep = self:GetWeaponByLabel('LightChronatronCannon')
+            -- wep:ChangeMaxRadius(bp.NewMaxRadius or 25)
+            -- local wep = self:GetWeaponByLabel('OverCharge')
+            -- wep:ChangeMaxRadius(bp.NewMaxRadius or 25)
+            -- local aoc = self:GetWeaponByLabel('AutoOverCharge')
+            -- aoc:ChangeMaxRadius(bp.NewMaxRadius or 25)
+        -- end
+    -- end,
 }
 
 TypeClass = XSL0301
